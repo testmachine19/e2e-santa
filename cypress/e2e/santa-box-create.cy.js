11fb4aeeeb888c46a443f96119a1f59aedae3095
testmachine19 <patysson@mail.ru>
2024-01-06 21:07:30 +0400

    Домашнее задание 6.1.2-thrid

--- cypress/e2e/santa-box-create.cy.js
+++ cypress/e2e/santa-box-create.cy.js
@@ -130,20 +130,20 @@ describe("user can create a box and run it", () => {
     ).click();
   });
 
-  // after("delete box", () => {
-  //   cy.visit("/login");
-  //   cy.login(users.userAutor.email, users.userAutor.password);
-  //   cy.get(
-  //     '.layout-1__header-wrapper-fixed > .layout-1__header > .header > .header__items > .layout-row-start > [href="/account/boxes"] > .header-item > .header-item__text > .txt--med'
-  //   ).click();
-  //   cy.get(":nth-child(1) > a.base--clickable > .user-card").first().click();
-  //   cy.get(
-  //     ".layout-1__header-wrapper-fixed > .layout-1__header-secondary > .header-secondary > .header-secondary__right-item > .toggle-menu-wrapper > .toggle-menu-button > .toggle-menu-button--inner"
-  //   ).click();
-  //   cy.contains("Архивация и удаление").click({ force: true });
-  //   cy.get(":nth-child(2) > .form-page-group__main > .frm-wrapper > .frm").type(
-  //     "Удалить коробку"
-  //   );
-  //   cy.get(".btn-service").click();
-  // });
+  after("delete box", () => {
+    cy.request({
+      method: "DELETE",
+      url: "https://staging.lpitko.ru/box/9LN8uh",
+      auth: {
+        username: users.userAutor.email,
+        password: users.userAutor.password,
+      },
+    })
+      .then((response) => {
+        expect(response.status).to.eq(204);
+      })
+      .catch((error) => {
+        console.error("API-удаление не удалось:", error);
+      });
+  });
 });
